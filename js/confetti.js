fd834c3fb7af2e7cc9d30215e040689da0211d58
MellowZtmy <tappynanakoro@gmail.com>
2024-06-17 01:19:32 +0900

    紙吹雪最終調整、html成形

--- js/confetti.js
+++ js/confetti.js
@@ -4,6 +4,7 @@ function dispConfetti() {
       window.requestAnimationFrame ||
       window.mozRequestAnimationFrame ||
       window.webkitRequestAnimationFrame;
+
     var canvas = document.querySelector('canvas');
     if (canvas) {
       canvas.width = window.innerWidth;
@@ -12,7 +13,8 @@ function dispConfetti() {
       ctx.globalCompositeOperation = 'source-over';
       var particles = [];
       var pIndex = 0;
-      var x, y, frameId;
+      var frameId;
+
       function Dot(x, y, vx, vy, color) {
         this.x = x;
         this.y = y;
@@ -28,7 +30,7 @@ function dispConfetti() {
         this.size = Math.floor(getRandom(5, 7));
       }
 
-      Dot.prototype.draw = function (x, y) {
+      Dot.prototype.draw = function () {
         this.degree += 1;
         this.vx *= 0.99;
         this.vy *= 0.999;
@@ -38,16 +40,13 @@ function dispConfetti() {
         this.height = Math.cos((this.degree * Math.PI) / 20) * this.size;
         ctx.fillStyle = this.color;
         ctx.beginPath();
-        ctx.moveTo(this.x + this.x / 2, this.y + this.y / 2);
+        ctx.moveTo(this.x + this.width / 2, this.y + this.height / 2);
         ctx.lineTo(
-          this.x + this.x / 2 + this.width / 2,
-          this.y + this.y / 2 + this.height
+          this.x + this.width / 2 + this.width / 2,
+          this.y + this.height
         );
-        ctx.lineTo(
-          this.x + this.x / 2 + this.width + this.width / 2,
-          this.y + this.y / 2 + this.height
-        );
-        ctx.lineTo(this.x + this.x / 2 + this.width, this.y + this.y / 2);
+        ctx.lineTo(this.x + this.width + this.width / 2, this.y + this.height);
+        ctx.lineTo(this.x + this.width, this.y);
         ctx.closePath();
         ctx.fill();
         this.life++;
@@ -55,40 +54,33 @@ function dispConfetti() {
           delete particles[this.id];
         }
       };
+
       window.addEventListener('resize', function () {
         canvas.width = window.innerWidth;
         canvas.height = window.innerHeight;
-        x = canvas.width / 1;
-        y = canvas.height / 1;
       });
 
       function loop() {
         ctx.clearRect(0, 0, canvas.width, canvas.height);
         if (frameId % 1 == 0) {
           new Dot(
-            canvas.width * Math.random() -
-              canvas.width +
-              canvas.width * Math.random(),
-            -canvas.height / 2,
-            getRandom(1, 3),
+            Math.random() * canvas.width,
+            -50,
+            getRandom(-3, 3),
             getRandom(2, 4),
             '#f9e7c3'
           );
           new Dot(
-            canvas.width * Math.random() -
-              canvas.width +
-              canvas.width * Math.random(),
-            -canvas.height / 2,
-            getRandom(1, 3),
+            Math.random() * canvas.width,
+            -50,
+            getRandom(-3, 3),
             getRandom(2, 4),
             '#e3b458'
           );
           new Dot(
-            canvas.width * Math.random() -
-              canvas.width +
-              canvas.width * Math.random(),
-            -canvas.height / 2,
-            getRandom(1, 3),
+            Math.random() * canvas.width,
+            -50,
+            getRandom(-3, 3),
             getRandom(2, 4),
             '#9d7d1d'
           );
